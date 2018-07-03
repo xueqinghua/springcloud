e188b56d4e8c4decba1154ac6411647b0dfc91ba
xueqinghua <xueqinghua@wywk.cn>
2018-07-03 18:45:32 +0800

    init project

---
+++ hystrix/src/main/java/com/sample/cloud/hystrix/App.java
@@ -0,0 +1,13 @@
+package com.sample.cloud.hystrix;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
